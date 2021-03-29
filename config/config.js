f1bd52335ec41d7a488ace47b1b7bab522bedf14
zealorez <zealorez@gmail.com>
2021-03-29 17:26:26 +0800

    first commit

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: '<YOUR_USER_NAME>',
-    password: null,
-    database: '<YOUR_APP_NAME>_development',
+    username: 'postgres',
+    password: 'password',
+    database: 'black_jack_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
